6ffa8db676bfc5481ea7826e5f4da0f0e09549f9
Ujjwal Singh Basnet <118468393+ujjwalbasnyat@users.noreply.github.com>
2025-03-01 06:27:11 +0000

    minute calculation debugged

--- oops/problems/seasons.py
+++ oops/problems/seasons.py
@@ -1,27 +1,31 @@
 import inflect
-from datetime import timedelta
-import datetime as dt
+import datetime
 p = inflect.engine()
+import sys
 
 class Student:
-    def __init__(self, total_date):
-        self.total_date = total_date
-        print(self.total_date)
+    def __init__(self, till_day):
+        self.till_day = till_day
+        print(self.till_day)
 
     
     @classmethod
     def get(cls):
-        birth_date = input("Date of Birth: ")
-        year, months, day = birth_date.split("-")
+        bday = input("Date of Birth: ")
+        if not "-" in bday:
+            return "Invalid date"
+        year, months, day = bday.split("-")
         year, months, day = int(year), int(months), int(day)
-        birth_date = dt.datetime(year, months, day)
-        current_date =  dt.datetime.now()
-        total_date = (current_date - birth_date)*24
-        num, string = total_date.split(" ")
-        into_words = p.number_to_words(int(num))
-        return cls(into_words)
+        bday = datetime.date(year, months, day)
+        tday=  datetime.date.today()
+        till_day = (tday - bday)*24
+        wday = p.number_to_words(till_day.days)
+        wday = wday.replace(" and ", " ") + " minutes"
+        return wday
+
 
 def main():
     print(Student.get())
+    sys.exit()
 
 main()
