c99f383cba29f40229c8ffb6b2b331ede5043702
Ujjwal Singh Basnet <118468393+ujjwalbasnyat@users.noreply.github.com>
2025-03-11 07:03:39 +0000

    updated text

--- pdf/shirtificate.py
+++ pdf/shirtificate.py
@@ -4,11 +4,8 @@ def main():
     pdf = FPDF()
     name = input("Enter name: ")
     pdf.add_page()
-    pdf.cell(30, 10 , "CS50 Shirtificate", align = "C")
-    pdf.ln(20)
     pdf.set_font('arial',size=16 )
-    pdf.text(50, 50, txt=f"{name}i took CS50")
-    pdf.ln(20)
+    pdf.text(50, 50, txt=f"{name} took CS50")
     pdf.image('shirtificate.png', w=20, h=20)
     pdf.output('random.pdf')
 
